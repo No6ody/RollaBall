4ecf8701ab7702a1d34c4949da669eb2d90af658
Claudia Lölkes <claudia.loelkes@mail.de>
2015-10-14 07:24:54 +0200

    Version 1 - 14.10.15

---
+++ Assets/_Scripts/PlayerControler.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerControler : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private int count;
+    private Rigidbody rb;
+
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+	}
+	
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(move*speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Assets/_Scripts/Rotator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+    private float speed = 10;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(15,30,45)*Time.deltaTime);
+	}
+}
